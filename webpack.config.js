73944ffc655c2b805d4267b5f9d60dd417df06e1
Sunil Patil <sunilpatilsnd@gmail.com>
2025-06-20 20:05:07 +0530

    initial setup created

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template, LoaderOptionsPlugin } = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
